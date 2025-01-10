7b07fb0c43d1737ffbadd9835b6ffb9e0b0c7d90
Jonathan De Groot <jonathan.degroot@focisolutions.com>
2025-01-09 19:03:01 -0500

    user server side action to pull data from database

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,10 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+// Convert prisma object to js object
+export function convertToPlainObject<T>(value: T): T {
+    return JSON.parse(JSON.stringify(value));
 }
